00a06ae57772a0c7313f386836887c436976a955
Jacob Long <jclong98@gmail.com>
2025-05-07 13:50:11 -0700

    change warning to show [Goose]

--- packages/vite/src/plugins/deduplication.ts
+++ packages/vite/src/plugins/deduplication.ts
@@ -8,14 +8,14 @@ export function duplicationDetectionPlugin(): PluginOption {
 
       if (plugins.filter((i) => i.name === "unplugin-auto-import").length > 1) {
         throw new Error(
-          "[UI] Multiple instances of `unplugin-auto-import` detected. goose includes `unplugin-auto-import` already, and you can configure it using `autoImport` option in goose module options."
+          "[Goose] Multiple instances of `unplugin-auto-import` detected. goose includes `unplugin-auto-import` already, and you can configure it using `autoImport` option in goose module options."
         );
       }
       if (
         plugins.filter((i) => i.name === "unplugin-vue-components").length > 1
       ) {
         throw new Error(
-          "[UI] Multiple instances of `unplugin-vue-components` detected. goose includes `unplugin-vue-components` already, and you can configure it using `components` option in goose module options."
+          "[Goose] Multiple instances of `unplugin-vue-components` detected. goose includes `unplugin-vue-components` already, and you can configure it using `components` option in goose module options."
         );
       }
     },
