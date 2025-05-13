aede8d00e504aef70a9cec9c72ca189f22f51493
Jacob Long <jclong98@gmail.com>
2025-05-12 19:12:24 -0700

    fix: correct title casing and restructure App.vue layout

--- packages/sandbox/src/App.vue
+++ packages/sandbox/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav class="flex justify-between p-4 border-b border-gray-500/50">
-    <span class="text-xl font-semibold">Goose Playground</span>
-
-    <ul class="flex gap-4">
-      <li>
-        <router-link active-class="underline" to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link active-class="underline" to="/about">About</router-link>
-      </li>
-    </ul>
-  </nav>
-
-  <main class="p-4">
-    <router-view />
-  </main>
+  <div class="max-w-prose mx-auto min-h-screen">
+    <nav class="flex justify-between p-4">
+      <span class="text-xl font-semibold">Goose Sandbox</span>
+      <ul class="flex gap-4">
+        <li>
+          <router-link active-class="underline" to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link active-class="underline" to="/about">About</router-link>
+        </li>
+      </ul>
+    </nav>
+    <main class="p-4">
+      <router-view />
+    </main>
+  </div>
 </template>
