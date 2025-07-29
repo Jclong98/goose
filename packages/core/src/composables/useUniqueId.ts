96f7adb2458d5c1727835f38baf2130f702d60c4
Jacob Long <jclong98@gmail.com>
2025-07-29 09:51:34 -0700

    refactor: improve useUniqueId to prioritize attrs.id over generated uniqueId

--- packages/core/src/composables/useUniqueId.ts
+++ packages/core/src/composables/useUniqueId.ts
@@ -1,8 +1,10 @@
 import { computed, type ComputedRef, useAttrs, useId } from "vue";
 
 export function useUniqueId(): ComputedRef<string> {
-  const id = useId();
   const attrs = useAttrs();
+  const attrId = attrs.id as string | undefined;
+  if (attrId) return computed(() => attrId);
 
-  return computed(() => (attrs.id as string) ?? id);
+  const uniqueId = useId();
+  return computed(() => uniqueId);
 }
