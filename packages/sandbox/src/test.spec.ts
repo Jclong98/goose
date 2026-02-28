2dbe5e77e6f23903a36f9d7345a658a26aa36c73
Jacob Long <jclong98@gmail.com>
2026-02-28 13:38:27 -0700

    formatting

--- packages/sandbox/src/test.spec.ts
+++ packages/sandbox/src/test.spec.ts
@@ -18,7 +18,9 @@ describe("globals?", () => {
       template: `<GButton>click me</GButton>`,
     });
 
-    expect(wrapper.html()).toMatchInlineSnapshot(`"<button class="bg-goose cursor-pointer rounded border-none px-4 py-1 text-white transition duration-200 ease-in-out hover:brightness-95 active:scale-95 active:brightness-75">click me</button>"`);
+    expect(wrapper.html()).toMatchInlineSnapshot(
+      `"<button class="bg-goose cursor-pointer rounded border-none px-4 py-1 text-white transition duration-200 ease-in-out hover:brightness-95 active:scale-95 active:brightness-75">click me</button>"`,
+    );
   });
 
   it("should be able to auto load vue directives from goose", () => {
