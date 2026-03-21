3ed361b6a043b61f017986e2d90eb4ca2986aecc
Jacob Long <jclong98@gmail.com>
2026-03-21 16:22:25 -0700

    dark buttons

--- src/components/GButton/GButton.vue
+++ src/components/GButton/GButton.vue
@@ -24,15 +24,15 @@ const props = withDefaults(
   transition: background-color 0.1s ease;
 
   &.--primary {
-    background-color: #ffe07b;
+    background-color: var(--color-goose);
     color: black;
 
     &:hover {
-      background-color: #ffd54f;
+      background-color: oklch(from var(--color-goose) 0.85 c h);
     }
 
     &:active {
-      background-color: #ffc107;
+      background-color: oklch(from var(--color-goose) 0.8 c h);
     }
   }
 
@@ -74,6 +74,59 @@ const props = withDefaults(
 
     &:active {
       color: #004085;
+    }
+  }
+}
+
+.dark .g-button {
+  &.--primary {
+    &:hover {
+      background-color: oklch(from var(--color-goose) 0.8 c h);
+    }
+
+    &:active {
+      background-color: oklch(from var(--color-goose) 0.7 c h);
+    }
+  }
+
+  &.--secondary {
+    background-color: #333;
+    color: white;
+    border: 1px solid #555;
+
+    &:hover {
+      background-color: #444;
+    }
+
+    &:active {
+      background-color: #555;
+    }
+  }
+
+  &.--ghost {
+    background-color: transparent;
+    color: white;
+
+    &:hover {
+      background-color: #333;
+    }
+
+    &:active {
+      background-color: #444;
+    }
+  }
+
+  &.--link {
+    background-color: transparent;
+    color: #66b2ff;
+    text-decoration: underline;
+
+    &:hover {
+      color: #3399ff;
+    }
+
+    &:active {
+      color: #007acc;
     }
   }
 }
