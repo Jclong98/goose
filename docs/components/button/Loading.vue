5d24e285dcb889d5b0457700ba2d3be0fe0619ba
Jacob Long <jclong98@gmail.com>
2026-03-23 11:30:02 -0700

    better loading slot example

--- docs/components/button/Loading.vue
+++ docs/components/button/Loading.vue
@@ -2,14 +2,11 @@
 
 <template>
   <div class="flex flex-wrap gap-2">
+    <GButton loading> Loading Button </GButton>
+
     <GButton loading>
       <template #icon> <span>🙂</span> </template>
-
-      Loading Button
-    </GButton>
-
-    <GButton loading>
-      <template #loading> <span class="animate-spin">WOAH</span> </template>
+      <template #loading> <span class="animate-spin">😵‍💫</span> </template>
       Custom Loading Slot
     </GButton>
   </div>
