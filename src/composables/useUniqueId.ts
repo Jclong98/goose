4cd023aecec08a2f0bf7807e42419df6bded1b39
Jacob Long <jclong98@gmail.com>
2026-03-22 19:09:32 -0700

    refactor: simplify unique ID computation logic

--- src/composables/useUniqueId.ts
+++ src/composables/useUniqueId.ts
@@ -2,9 +2,9 @@ import { computed, type ComputedRef, useAttrs, useId } from "vue";
 
 export function useUniqueId(): ComputedRef<string> {
   const attrs = useAttrs();
-  const attrId = attrs.id as string | undefined;
-  if (attrId) return computed(() => attrId);
-
   const uniqueId = useId();
-  return computed(() => uniqueId);
+  return computed(() => {
+    const attrId = attrs.id as string | undefined;
+    return attrId || uniqueId;
+  });
 }
